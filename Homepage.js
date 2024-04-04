d02e91f3aaa00d60545366c45c799b61efdcd00c
ShaneDaniel484 <103937168+ShaneDaniel484@users.noreply.github.com>
2024-04-04 16:06:28 +0530

    audio when moved feature

--- Homepage.js
+++ Homepage.js
@@ -1,14 +1,17 @@
 var inputField = document.getElementById("name");
 var isMoved = false; // Flag to track if input box is moved
+var moveSound = document.getElementById("moveSound");
 
 inputField.addEventListener("click", function() {
     if (!isMoved) {
         inputField.classList.add("input-move");
+        moveSound.play();
     } else {
         inputField.classList.remove("input-move");
         // Trigger reflow before adding animation class again
         void inputField.offsetWidth;
         inputField.classList.add("input-moveBack");
+        moveSound.play();
     }
     isMoved = !isMoved; // Toggle the flag
 });
